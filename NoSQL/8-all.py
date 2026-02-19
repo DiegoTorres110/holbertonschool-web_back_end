ce4e03159e0a4d1d7bfe35f74a7f4d1198fb8de8
DiegoTorres110 <towers9090@gmail.com>
2026-02-18 23:08:35 -0600

    Update 8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""8-all"""
+
 def list_all(mongo_collection):
+    """Lists all documents in a collection"""
     documents = list(mongo_collection.find())
     if documents == 0:
         return []
