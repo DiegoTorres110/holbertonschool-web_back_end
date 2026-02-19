d0f596134569574b39e3ef5b2fc9064dc25cbf6b
DiegoTorres110 <towers9090@gmail.com>
2026-02-18 23:02:21 -0600

    Update list_all function to handle empty collections
    Fix function to return an empty list when no documents are found.

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-//Act8
+#Act8
 def list_all(mongo_collection):
-  x = mongo_collection.find()
-  if x.count() == 0:
-    return (x)
+    x = mongo_collection.find()
+    if x.count() == 0:
+      return[]
+    else:
+      return x
