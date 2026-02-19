b5a99a9fad90622404112d3f644e3822fd8959f6
DiegoTorres110 <towers9090@gmail.com>
2026-02-18 22:48:20 -0600

    Clean up docstring in 8-all.py
    Removed unnecessary docstring from list_all function.

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,4 @@
 //Act8
 #!/usr/bin/env python3
-"""Module that lists all documents in a MongoDB collection"""
-
 def list_all(mongo_collection):
-  """Return a list of all documents in the collection"""
   return list (mongo_collection.find())
