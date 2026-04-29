6541a61f8bd3cc462c7fee910922c07abfb83527
DiegoTorres110 <towers9090@gmail.com>
2026-04-28 22:19:49 -0600

    Update 9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
-Module for task 9: Insert a document in Python
+Module for task 9: Insert a document in Python using PyMongo
 """
 
 
 def insert_school(mongo_collection, **kwargs):
     """
     Inserts a new document in a collection based on kwargs
+    Returns the new _id of the inserted document
     """
-    return mongo_collection.insert_one(kwargs).inserted_id
+    new_document = mongo_collection.insert_one(kwargs)
+    return new_document.inserted_id
