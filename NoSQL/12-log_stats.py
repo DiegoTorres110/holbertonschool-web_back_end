a5862e544980e4a110a1d55df329fdc6d4ab0a07
DiegoTorres110 <towers9090@gmail.com>
2026-04-28 22:32:58 -0600

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,14 +2,13 @@
 """
 Provides some stats about Nginx logs stored in MongoDB
 """
-from pymongo import MongoClient 
-from typing import List, Dict, Optional
+from pymongo import MongoClient
+from typing import List, Dict
 
 
 def count(collection, options: Dict[str, str] = {}) -> int:
     """
     Take a mongoDB collection, filter data and count it
-
     Args:
         collection: MongoDB collection
         options (Dict[str, str]): options to filter
@@ -21,16 +20,14 @@ if __name__ == '__main__':
     client = MongoClient(host="localhost", port=27017)
     collection = client.logs.nginx
     methods: List[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
     print("{:d} logs".format(count(collection)))
     print("Methods:")
-
     for method in methods:
         print("\tmethod {:s}: {:d}".format(
             method,
             count(collection, {"method": method})
         ))
-
     print("{:d} status check".format(
         count(collection, {"method": "GET", "path": "/status"})
     ))
+    
