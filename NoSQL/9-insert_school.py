7d81f0b7cfaaa1c1d4e37f53d6e834422e58b125
DiegoTorres110 <towers9090@gmail.com>
2026-04-28 22:13:34 -0600

    Update 9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Write a Python function that inserts a new document in a
-    collection based on kwarg
+    collection based on kwargs
 """
 
 
@@ -8,5 +8,8 @@ def insert_school(mongo_collection, **kwargs):
     """ mongo_collection will be the pymongo collection object
         Returns the new _id
     """
-    document_id = mongo_collection.insert(kwargs)
-    return document_id
+    # Use insert_one for a single document
+    result = mongo_collection.insert_one(kwargs)
+    
+    # Return specifically the inserted_id attribute
+    return result.inserted_id
